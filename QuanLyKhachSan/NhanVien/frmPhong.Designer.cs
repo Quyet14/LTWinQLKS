ae0797ab94b7325deb0da41b0f601f8b9f584af9
TanTai311 <tanteiuwu@gmail.com>
2024-10-28 00:32:37 +0700

    frmPhong

---
+++ QuanLyKhachSan/NhanVien/frmPhong.Designer.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan.NhanVien
+{
+    partial class frmPhong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPhong = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMaPhong = new System.Windows.Forms.TextBox();
+            this.cbMaLoaiPhong = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtGia = new System.Windows.Forms.TextBox();
+            this.cbMaTinhTrang = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPhong)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPhong
+            // 
+            this.dgvPhong.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvPhong.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPhong.Location = new System.Drawing.Point(51, 592);
+            this.dgvPhong.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvPhong.Name = "dgvPhong";
+            this.dgvPhong.RowHeadersWidth = 51;
+            this.dgvPhong.RowTemplate.Height = 24;
+            this.dgvPhong.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPhong.Size = new System.Drawing.Size(1971, 878);
+            this.dgvPhong.TabIndex = 0;
+            this.dgvPhong.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPhong_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(336, 52);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Quản Lý Phòng";
+            // 
+            // txtMaPhong
+            // 
+            this.txtMaPhong.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaPhong.Location = new System.Drawing.Point(279, 178);
+            this.txtMaPhong.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtMaPhong.Name = "txtMaPhong";
+            this.txtMaPhong.Size = new System.Drawing.Size(556, 44);
+            this.txtMaPhong.TabIndex = 2;
+            // 
+            // cbMaLoaiPhong
+            // 
+            this.cbMaLoaiPhong.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbMaLoaiPhong.FormattingEnabled = true;
+            this.cbMaLoaiPhong.Location = new System.Drawing.Point(279, 316);
+            this.cbMaLoaiPhong.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbMaLoaiPhong.Name = "cbMaLoaiPhong";
+            this.cbMaLoaiPhong.Size = new System.Drawing.Size(556, 45);
+            this.cbMaLoaiPhong.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(772, 494);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(213, 70);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(1138, 494);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(213, 70);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Sửa";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 178);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã phòng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(52, 328);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(219, 37);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Mã loại phòng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1077, 178);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 37);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Giá";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1077, 328);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(227, 37);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Mã Tình Trạng";
+            // 
+            // txtGia
+            // 
+            this.txtGia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGia.Location = new System.Drawing.Point(1311, 178);
+            this.txtGia.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtGia.Name = "txtGia";
+            this.txtGia.Size = new System.Drawing.Size(604, 44);
+            this.txtGia.TabIndex = 2;
+            // 
+            // cbMaTinhTrang
+            // 
+            this.cbMaTinhTrang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbMaTinhTrang.FormattingEnabled = true;
+            this.cbMaTinhTrang.Location = new System.Drawing.Point(1311, 328);
+            this.cbMaTinhTrang.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbMaTinhTrang.Name = "cbMaTinhTrang";
+            this.cbMaTinhTrang.Size = new System.Drawing.Size(604, 45);
+            this.cbMaTinhTrang.TabIndex = 3;
+            // 
+            // frmPhong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(2066, 1573);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cbMaTinhTrang);
+            this.Controls.Add(this.cbMaLoaiPhong);
+            this.Controls.Add(this.txtGia);
+            this.Controls.Add(this.txtMaPhong);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvPhong);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmPhong";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmPhong_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPhong)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPhong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMaPhong;
+        private System.Windows.Forms.ComboBox cbMaLoaiPhong;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtGia;
+        private System.Windows.Forms.ComboBox cbMaTinhTrang;
+    }
+}
---
+++ QuanLyKhachSan/NhanVien/frmPhong.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using DoAnN6_QLKS_DAL.Entity;
+
+namespace QuanLyKhachSan.NhanVien
+{
+    public partial class frmPhong : Form
+    {
+        Model1 context = new Model1();
+        public frmPhong()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPhong_Load(object sender, EventArgs e)
+        {
+            LoadDataFromDatabase();
+            LoadDataToComboBox();
+        }
+
+        private void LoadDataToComboBox()
+        {
+            // Chuỗi kết nối SQL Server
+            string connectionString = @"Server=THANH_TUNG;Database=QLKS;Trusted_Connection=True;";
+
+            // Query cho ComboBox LoaiPhong
+            string queryLoaiPhong = "SELECT MaLoaiPhong, TenLoaiPhong FROM LoaiPhong";
+
+            // Query cho ComboBox TinhTrangPhong
+            string queryTinhTrangPhong = "SELECT MaTinhTrang, TenTinhTrang FROM LoaiTinhTrangPhong";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open(); // Mở kết nối SQL
+
+                    // Đổ dữ liệu vào ComboBox LoaiPhong
+                    SqlDataAdapter daLoaiPhong = new SqlDataAdapter(queryLoaiPhong, connection);
+                    DataTable dtLoaiPhong = new DataTable();
+                    daLoaiPhong.Fill(dtLoaiPhong);
+                    cbMaLoaiPhong.DataSource = dtLoaiPhong;
+                    cbMaLoaiPhong.DisplayMember = "TenLoaiPhong"; // Hiển thị tên loại phòng
+                    cbMaLoaiPhong.ValueMember = "MaLoaiPhong"; // Giá trị là mã loại phòng
+
+                    // Đổ dữ liệu vào ComboBox TinhTrangPhong
+                    SqlDataAdapter daTinhTrangPhong = new SqlDataAdapter(queryTinhTrangPhong, connection);
+                    DataTable dtTinhTrangPhong = new DataTable();
+                    daTinhTrangPhong.Fill(dtTinhTrangPhong);
+                    cbMaTinhTrang.DataSource = dtTinhTrangPhong;
+                    cbMaTinhTrang.DisplayMember = "TenTinhTrang"; // Hiển thị tên tình trạng phòng
+                    cbMaTinhTrang.ValueMember = "MaTinhTrang"; // Giá trị là mã tình trạng phòng
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi tải dữ liệu vào ComboBox: " + ex.Message);
+                }
+            }
+        }
+
+        private void LoadDataFromDatabase()
+        {
+            // Chuỗi kết nối SQL Server
+            string connectionString = @"Server=THANH_TUNG;Database=QLKS;Trusted_Connection=True;";
+
+            // Câu truy vấn với JOIN
+            string query = @"
+                         SELECT p.*, lp.TenLoaiPhong, tt.TenTinhTrang
+                         FROM Phong p
+                         JOIN LoaiPhong lp ON p.MaLoaiPhong = lp.MaLoaiPhong
+                         JOIN LoaiTinhTrangPhong tt ON p.MaTinhTrang = tt.MaTinhTrang";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open(); // Mở kết nối SQL
+
+                    SqlDataAdapter da = new SqlDataAdapter(query, connection);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dgvPhong.DataSource = dt; // Gán dữ liệu vào DataGridView
+
+                    // Tùy chỉnh hiển thị cột (nếu cần)
+                    dgvPhong.Columns["MaLoaiPhong"].Visible = false; // Ẩn cột mã loại phòng
+                    dgvPhong.Columns["MaTinhTrang"].Visible = false; // Ẩn cột mã tình trạng phòng
+
+                    // Đặt tên cột cho dễ hiểu
+                    dgvPhong.Columns["TenLoaiPhong"].HeaderText = "Loại Phòng";
+                    dgvPhong.Columns["TenTinhTrang"].HeaderText = "Tình Trạng Phòng";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi tải dữ liệu vào DataGridView: " + ex.Message);
+                }
+            }
+
+
+
+            //string connectionString = @"Server=THANH_TUNG;Database=QLKS;Trusted_Connection=True;"; // Chuỗi kết nối
+            //string query = "SELECT * FROM Phong"; // Câu lệnh SQL để lấy dữ liệu
+
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    try
+            //    {
+            //        connection.Open(); // Mở kết nối
+
+            //        // Tạo đối tượng SqlDataAdapter để đổ dữ liệu vào DataTable
+            //        SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+
+            //        // Tạo đối tượng DataTable để chứa dữ liệu
+            //        DataTable dataTable = new DataTable();
+
+            //        // Đổ dữ liệu từ SQL vào DataTable
+            //        dataAdapter.Fill(dataTable);
+
+            //        // Gán DataTable vào DataGridView
+            //        dgvPhong.DataSource = dataTable;
+
+            //        // Tùy chọn: Tự động điều chỉnh kích thước cột cho vừa nội dung
+            //        dgvPhong.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        MessageBox.Show("Lỗi khi tải dữ liệu: " + ex.Message);
+            //    }
+            //}        }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra thông tin và giá
+            if (!IsInputValid() || !IsPriceValid())
+            {
+                return; // Dừng xử lý nếu có lỗi
+            }
+
+            DataTable dt = (DataTable)dgvPhong.DataSource;
+            DataRow newRow = dt.NewRow();
+
+            // Gán giá trị cho các cột
+            newRow["MaPhong"] = txtMaPhong.Text; // Số phòng từ một textbox
+            newRow["MaLoaiPhong"] = cbMaLoaiPhong.SelectedValue; // Mã loại phòng từ combobox
+            newRow["MaTinhTrang"] = cbMaTinhTrang.SelectedValue; // Mã tình trạng phòng từ combobox
+            newRow["GiaPhong"] = decimal.Parse(txtGia.Text); // Giá phòng từ textbox
+                                                             // Gán thêm các giá trị khác nếu cần
+
+            dt.Rows.Add(newRow); // Thêm dòng mới vào DataTable
+            if (UpdateDatabase(dt))
+            {
+                MessageBox.Show("Thêm phòng thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Lỗi khi thêm phòng!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            int selectedRowIndex = dgvPhong.CurrentCell.RowIndex;
+            if (selectedRowIndex >= 0)
+            {
+
+                // Kiểm tra thông tin và giá
+                if (!IsInputValid() || !IsPriceValid())
+                {
+                    return; // Dừng xử lý nếu có lỗi
+                }
+
+                DataTable dt = (DataTable)dgvPhong.DataSource;
+                DataRow row = dt.Rows[selectedRowIndex];
+
+                // Cập nhật giá trị
+                row["MaPhong"] = txtMaPhong.Text;
+                row["MaLoaiPhong"] = cbMaLoaiPhong.SelectedValue;
+                row["MaTinhTrang"] = cbMaTinhTrang.SelectedValue;
+                row["GiaPhong"] = decimal.Parse(txtGia.Text); // Cập nhật giá phòng
+                                                              // Cập nhật thêm các giá trị khác nếu cần
+
+                if (UpdateDatabase(dt))
+                {
+                    MessageBox.Show("Sửa phòng thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Lỗi khi sửa phòng!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+          private bool UpdateDatabase(DataTable dt)
+           {
+          string connectionString = @"Server=THANH_TUNG;Database=QLKS;Trusted_Connection=True;";
+
+          using (SqlConnection connection = new SqlConnection(connectionString))
+          {
+              connection.Open();
+              SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM Phong", connection);
+              SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+
+              try
+              {
+                  // Cập nhật cơ sở dữ liệu
+                  dataAdapter.Update(dt);
+                  return true; // Trả về true nếu cập nhật thành công
+              }
+              catch (Exception)
+              {
+                  return false; // Trả về false nếu có lỗi
+              }
+            }
+        }
+
+        private void dgvPhong_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selectedRowIndex = dgvPhong.CurrentCell.RowIndex;
+            if (selectedRowIndex >= 0)
+            {
+
+                // Kiểm tra thông tin và giá
+                if (!IsInputValid() || !IsPriceValid())
+                {
+                    return; // Dừng xử lý nếu có lỗi
+                }
+
+                DataTable dt = (DataTable)dgvPhong.DataSource;
+                DataRow row = dt.Rows[selectedRowIndex];
+
+                // Cập nhật giá trị
+                row["MaPhong"] = txtMaPhong.Text;
+                row["MaLoaiPhong"] = cbMaLoaiPhong.SelectedValue;
+                row["MaTinhTrang"] = cbMaTinhTrang.SelectedValue;
+                row["GiaPhong"] = decimal.Parse(txtGia.Text); // Cập nhật giá phòng
+                                                              // Cập nhật thêm các giá trị khác nếu cần
+
+                if (UpdateDatabase(dt))
+                {
+                    MessageBox.Show("Sửa phòng thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Lỗi khi sửa phòng!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private bool IsInputValid()
+        {
+            if (string.IsNullOrWhiteSpace(txtMaPhong.Text) || cbMaLoaiPhong.SelectedValue == null ||
+                cbMaTinhTrang.SelectedValue == null)
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false; // Trả về false nếu thông tin không hợp lệ
+            }
+            return true; // Trả về true nếu thông tin hợp lệ
+        }
+
+        private bool IsPriceValid()
+        {
+            if (!decimal.TryParse(txtGia.Text, out decimal giaPhong))
+            {
+                MessageBox.Show("Giá phòng phải là số!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false; // Trả về false nếu giá không hợp lệ
+            }
+            return true; // Trả về true nếu giá hợp lệ
+        }
+    }
+}
