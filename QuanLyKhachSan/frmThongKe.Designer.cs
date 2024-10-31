fab91772eddff20107d88fff65552c97d01a7648
Quyet14 <tranquyetz098@gmail.com>
2024-10-31 11:47:46 +0700

    them

---
+++ QuanLyKhachSan/frmThongKe.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class frmThongKe
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.cbbThang = new System.Windows.Forms.ComboBox();
+            this.cbbNam = new System.Windows.Forms.ComboBox();
+            this.roundedPanel3 = new QuanLyKhachSan.Components.RoundedPanel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.roundedPanel2 = new QuanLyKhachSan.Components.RoundedPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.roundedPanel1 = new QuanLyKhachSan.Components.RoundedPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.roundedPanel3.SuspendLayout();
+            this.roundedPanel2.SuspendLayout();
+            this.roundedPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(24, 150);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 300);
+            this.panel1.TabIndex = 3;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(440, 150);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SemiTransparent;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Doanh thu phòng";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Doanh thu DV";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Tổng doanh thu";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(400, 300);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // cbbThang
+            // 
+            this.cbbThang.FormattingEnabled = true;
+            this.cbbThang.Location = new System.Drawing.Point(24, 118);
+            this.cbbThang.Name = "cbbThang";
+            this.cbbThang.Size = new System.Drawing.Size(121, 24);
+            this.cbbThang.TabIndex = 11;
+            this.cbbThang.Text = "Chọn tháng";
+            this.cbbThang.SelectedIndexChanged += new System.EventHandler(this.cbbThang_SelectedIndexChanged);
+            // 
+            // cbbNam
+            // 
+            this.cbbNam.FormattingEnabled = true;
+            this.cbbNam.Location = new System.Drawing.Point(450, 120);
+            this.cbbNam.Name = "cbbNam";
+            this.cbbNam.Size = new System.Drawing.Size(121, 24);
+            this.cbbNam.TabIndex = 12;
+            this.cbbNam.Text = "Chọn năm";
+            this.cbbNam.SelectedIndexChanged += new System.EventHandler(this.cbbNam_SelectedIndexChanged);
+            // 
+            // roundedPanel3
+            // 
+            this.roundedPanel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.roundedPanel3.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.roundedPanel3.BorderRadius = 20;
+            this.roundedPanel3.BorderSize = 0;
+            this.roundedPanel3.Controls.Add(this.label5);
+            this.roundedPanel3.Controls.Add(this.label6);
+            this.roundedPanel3.Location = new System.Drawing.Point(596, 12);
+            this.roundedPanel3.Name = "roundedPanel3";
+            this.roundedPanel3.Size = new System.Drawing.Size(195, 89);
+            this.roundedPanel3.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(170, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Số lượng phòng đặt";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(12, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "0 Phòng";
+            // 
+            // roundedPanel2
+            // 
+            this.roundedPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.roundedPanel2.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.roundedPanel2.BorderRadius = 20;
+            this.roundedPanel2.BorderSize = 0;
+            this.roundedPanel2.Controls.Add(this.label4);
+            this.roundedPanel2.Controls.Add(this.label3);
+            this.roundedPanel2.Location = new System.Drawing.Point(320, 12);
+            this.roundedPanel2.Name = "roundedPanel2";
+            this.roundedPanel2.Size = new System.Drawing.Size(188, 89);
+            this.roundedPanel2.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(11, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "0,000 VND";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(11, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Doanh thu dịch vụ";
+            // 
+            // roundedPanel1
+            // 
+            this.roundedPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.roundedPanel1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.roundedPanel1.BorderRadius = 20;
+            this.roundedPanel1.BorderSize = 0;
+            this.roundedPanel1.Controls.Add(this.label1);
+            this.roundedPanel1.Controls.Add(this.label2);
+            this.roundedPanel1.Location = new System.Drawing.Point(35, 12);
+            this.roundedPanel1.Name = "roundedPanel1";
+            this.roundedPanel1.Size = new System.Drawing.Size(180, 89);
+            this.roundedPanel1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(15, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Doanh thu phòng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(15, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "0,000 VND";
+            // 
+            // frmThongKe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 481);
+            this.Controls.Add(this.roundedPanel3);
+            this.Controls.Add(this.roundedPanel2);
+            this.Controls.Add(this.roundedPanel1);
+            this.Controls.Add(this.cbbNam);
+            this.Controls.Add(this.cbbThang);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmThongKe";
+            this.Text = "frmThongKe";
+            this.Load += new System.EventHandler(this.frmThongKe_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.roundedPanel3.ResumeLayout(false);
+            this.roundedPanel3.PerformLayout();
+            this.roundedPanel2.ResumeLayout(false);
+            this.roundedPanel2.PerformLayout();
+            this.roundedPanel1.ResumeLayout(false);
+            this.roundedPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbbThang;
+        private System.Windows.Forms.ComboBox cbbNam;
+        private Components.RoundedPanel roundedPanel1;
+        private Components.RoundedPanel roundedPanel2;
+        private Components.RoundedPanel roundedPanel3;
+    }
+}
---
+++ QuanLyKhachSan/frmThongKe.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using DoAnN6_QLKS_DAL.Entity;
+
+namespace QuanLyKhachSan
+{
+    public partial class frmThongKe : Form
+    {
+        private QuanLyKhachSanDB db;
+
+        public frmThongKe()
+        {
+            InitializeComponent();
+            db = new QuanLyKhachSanDB();
+            LoadInitialData();
+        }
+
+        private void LoadInitialData()
+        {
+            // Load dữ liệu ban đầu cho ComboBox
+            for (int i = 1; i <= 12; i++)
+            {
+                cbbThang.Items.Add(i.ToString());
+            }
+
+            var currentYear = DateTime.Now.Year;
+            for (int year = currentYear - 2; year <= currentYear; year++)
+            {
+                cbbNam.Items.Add(year.ToString());
+            }
+
+            // Set default selected values
+            cbbThang.SelectedIndex = DateTime.Now.Month - 1;
+            cbbNam.SelectedIndex = 2; // Năm hiện tại
+
+            // Load initial data
+            LoadData(DateTime.Now.Month, DateTime.Now.Year);
+        }
+
+        private void LoadData(int month, int year)
+        {
+            try
+            {
+                var doanhThuPhong = TinhDoanhThuPhong(month, year);
+                var doanhThuDichVu = TinhDoanhThuDichVu(month, year);
+                var soLuongPhongDat = DemSoPhongDat(month, year);
+
+                label2.Text = $"{doanhThuPhong:#,##0} VND";
+                label4.Text = $"{doanhThuDichVu:#,##0} VND";
+                label6.Text = $"{soLuongPhongDat} Phòng";
+
+                UpdatePieChart(doanhThuPhong, doanhThuDichVu);
+                UpdateLineChart(year); // Đảm bảo rằng năm được truyền vào đúng
+
+                // Cập nhật lại form
+                this.Refresh();
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllText("error_log.txt", $"{DateTime.Now}: {ex.Message}\n");
+                MessageBox.Show($"Có lỗi xảy ra: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private decimal TinhDoanhThuPhong(int month, int year)
+        {
+            return db.PhieuDatPhongs
+                .Where(p => p.NgayTraPhong.HasValue &&
+                            p.NgayTraPhong.Value.Month == month &&
+                            p.NgayTraPhong.Value.Year == year)
+                .Sum(p => (decimal?)(p.TongTien ?? 0)) ?? 0;
+        }
+
+        private decimal TinhDoanhThuDichVu(int month, int year)
+        {
+            return db.HoaDonDichVus
+                .Where(h => h.NgayLap.Month == month &&
+                            h.NgayLap.Year == year)
+                .Sum(h => (decimal?)(h.TongTien ?? 0)) ?? 0;
+        }
+
+        private int DemSoPhongDat(int month, int year)
+        {
+            return db.PhieuDatPhongs
+                .Where(p => p.NgayTraPhong.HasValue &&
+                            p.NgayTraPhong.Value.Month == month &&
+                            p.NgayTraPhong.Value.Year == year &&
+                            p.MaPhong != null)
+                .Select(p => p.MaPhong)
+                .Distinct()
+                .Count();
+        }
+
+
+        private void UpdatePieChart(decimal doanhThuPhong, decimal doanhThuDichVu)
+        {
+            try
+            {
+                panel1.Controls.Clear();
+                var pieChart = new Chart();
+                pieChart.Dock = DockStyle.Fill;
+
+                // Thêm ChartArea
+                var chartArea = new ChartArea();
+                chartArea.BackColor = Color.White;
+                pieChart.ChartAreas.Add(chartArea);
+
+                // Tạo series cho biểu đồ tròn
+                var series = new Series
+                {
+                    ChartType = SeriesChartType.Pie,
+                    IsValueShownAsLabel = true,
+                    LabelFormat = "{0:P1}"
+                };
+
+                var total = doanhThuPhong + doanhThuDichVu;
+                if (total > 0)
+                {
+                    // Thêm dữ liệu và cấu hình cho từng điểm
+                    series.Points.AddXY("Doanh thu phòng", (double)doanhThuPhong);
+                    series.Points[series.Points.Count - 1].Color = Color.FromArgb(255, 128, 128);
+                    series.Points[series.Points.Count - 1].LabelForeColor = Color.White;
+
+                    series.Points.AddXY("Doanh thu dịch vụ", (double)doanhThuDichVu);
+                    series.Points[series.Points.Count - 1].Color = Color.FromArgb(128, 255, 128);
+                    series.Points[series.Points.Count - 1].LabelForeColor = Color.White;
+                }
+                else
+                {
+                    series.Points.AddXY("Không có dữ liệu", 1);
+                    series.Points[series.Points.Count - 1].Color = Color.LightGray;
+                    series.Points[series.Points.Count - 1].LabelForeColor = Color.White;
+                }
+
+                // Thêm series vào chart
+                pieChart.Series.Add(series);
+
+                // Cấu hình Legend (chú thích)
+                var legend = new Legend
+                {
+                    Docking = Docking.Bottom,
+                    Alignment = StringAlignment.Center
+                };
+                pieChart.Legends.Add(legend);
+
+                // Thêm tiêu đề
+                var title = new Title("Tỷ lệ doanh thu")
+                {
+                    Font = new Font("Microsoft Sans Serif", 12, FontStyle.Bold),
+                    ForeColor = Color.Black,
+                    Docking = Docking.Top
+                };
+                pieChart.Titles.Add(title);
+
+                // Cấu hình thêm cho chart
+                pieChart.AntiAliasing = AntiAliasingStyles.All;
+                pieChart.TextAntiAliasingQuality = TextAntiAliasingQuality.High;
+
+                // Đặt kích thước biểu đồ tròn
+                series.ChartArea = chartArea.Name;
+                chartArea.InnerPlotPosition.Auto = false;
+                chartArea.InnerPlotPosition.Width = 85f;
+                chartArea.InnerPlotPosition.Height = 85f;
+                chartArea.InnerPlotPosition.X = 7.5f;
+                chartArea.InnerPlotPosition.Y = 5f;
+
+                // Thêm chart vào panel
+                panel1.Controls.Add(pieChart);
+
+                // Cập nhật hiển thị
+                pieChart.Invalidate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi cập nhật biểu đồ tròn: {ex.Message}", "Lỗi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void UpdateLineChart(int year)
+        {
+            try
+            {
+                // Xóa tất cả series hiện có
+                chart1.Series.Clear();
+
+                // Tạo các series mới với màu sắc tương ứng với các panel
+                var series1 = new Series("Doanh thu phòng")
+                {
+                    ChartType = SeriesChartType.Line,
+                    BorderWidth = 3,
+                    Color = Color.FromArgb(255, 128, 128), // Màu đỏ nhạt như roundedPanel1
+                    MarkerStyle = MarkerStyle.Circle,
+                    MarkerSize = 8
+                };
+
+                var series2 = new Series("Doanh thu DV")
+                {
+                    ChartType = SeriesChartType.Line,
+                    BorderWidth = 3,
+                    Color = Color.FromArgb(128, 255, 128), // Màu xanh lá như roundedPanel2
+                    MarkerStyle = MarkerStyle.Circle,
+                    MarkerSize = 8
+                };
+
+                var series3 = new Series("Tổng doanh thu")
+                {
+                    ChartType = SeriesChartType.Line,
+                    BorderWidth = 3,
+                    Color = Color.FromArgb(128, 128, 255), // Màu xanh dương như roundedPanel3
+                    MarkerStyle = MarkerStyle.Circle,
+                    MarkerSize = 8
+                };
+
+                bool hasData = false;
+                decimal maxValue = 0;
+
+                // Thêm dữ liệu cho từng tháng
+                for (int month = 1; month <= 12; month++)
+                {
+                    var doanhThuPhong = TinhDoanhThuPhong(month, year);
+                    var doanhThuDV = TinhDoanhThuDichVu(month, year);
+                    var tongDoanhThu = doanhThuPhong + doanhThuDV;
+
+                    series1.Points.AddXY(month, doanhThuPhong);
+                    series2.Points.AddXY(month, doanhThuDV);
+                    series3.Points.AddXY(month, tongDoanhThu);
+
+                    maxValue = Math.Max(maxValue, tongDoanhThu);
+
+                    if (doanhThuPhong > 0 || doanhThuDV > 0)
+                    {
+                        hasData = true;
+                    }
+                }
+
+                // Thêm series vào chart
+                chart1.Series.Add(series1);
+                chart1.Series.Add(series2);
+                chart1.Series.Add(series3);
+
+                // Cấu hình ChartArea
+                var chartArea = chart1.ChartAreas[0];
+                chartArea.AxisX.Interval = 1;
+                chartArea.AxisX.Minimum = 1;
+                chartArea.AxisX.Maximum = 12;
+                chartArea.AxisX.Title = "Tháng";
+                chartArea.AxisX.MajorGrid.LineColor = Color.LightGray;
+                chartArea.AxisX.LabelStyle.Font = new Font("Microsoft Sans Serif", 9);
+
+                // Định dạng trục Y
+                chartArea.AxisY.LabelStyle.Format = "{0:#,##0}";
+                chartArea.AxisY.Title = "Doanh thu (VNĐ)";
+                chartArea.AxisY.MajorGrid.LineColor = Color.LightGray;
+                chartArea.AxisY.LabelStyle.Font = new Font("Microsoft Sans Serif", 9);
+
+                // Điều chỉnh khoảng giá trị trục Y
+                if (maxValue > 0)
+                {
+                    chartArea.AxisY.Maximum = Math.Ceiling(Convert.ToDouble(maxValue) * 1.1);
+                }
+
+                // Cấu hình Legend (chú thích)
+                chart1.Legends[0].Docking = Docking.Bottom;
+                chart1.Legends[0].Alignment = StringAlignment.Center;
+                chart1.Legends[0].Font = new Font("Microsoft Sans Serif", 9);
+
+                // Cấu hình tiêu đề
+                chart1.Titles.Clear();
+                var title = new Title($"Biểu đồ doanh thu năm {year}")
+                {
+                    Font = new Font("Microsoft Sans Serif", 12, FontStyle.Bold),
+                    ForeColor = Color.Black
+                };
+                chart1.Titles.Add(title);
+
+                // Hiển thị thông báo nếu không có dữ liệu
+                if (!hasData)
+                {
+                    var noDataTitle = new Title("Không có dữ liệu")
+                    {
+                        Font = new Font("Microsoft Sans Serif", 10, FontStyle.Italic),
+                        ForeColor = Color.Gray,
+                        Docking = Docking.Top
+                    };
+                    chart1.Titles.Add(noDataTitle);
+                }
+
+                // Cập nhật hiển thị
+                chart1.Invalidate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi cập nhật biểu đồ: {ex.Message}", "Lỗi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void cbbThang_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbbThang.SelectedItem != null && cbbNam.SelectedItem != null)
+            {
+                int month = int.Parse(cbbThang.SelectedItem.ToString());
+                int year = int.Parse(cbbNam.SelectedItem.ToString());
+                LoadData(month, year);
+            }
+        }
+
+        private void cbbNam_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbbThang.SelectedItem != null && cbbNam.SelectedItem != null)
+            {
+                int month = int.Parse(cbbThang.SelectedItem.ToString());
+                int year = int.Parse(cbbNam.SelectedItem.ToString());
+                LoadData(month, year);
+            }
+        }
+
+        private void frmThongKe_Load_1(object sender, EventArgs e)
+        {
+            // Form đã được load trong constructor
+        }
+    }
+}
